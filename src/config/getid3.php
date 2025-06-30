adeb6055573077608e4508ba832ff7a3d68ef923
plutuss <dima92konovalenko@gmail.com>
2025-06-30 23:10:40 +0200

    key_prefix

--- src/config/getid3.php
+++ src/config/getid3.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    'key_prefix' => 'getid3', // Prefix for cache keys
     'cache' => env('GETID3_CACHE', true), // Enable or disable caching
     'cache_time' => env('GETID3_CACHE_TIME', 86400), // 1 day
 ];
